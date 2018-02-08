e663409849b33a0f2b5a8094dfb1cfc4f5877dcc
Sivakumar <sivakumar@tendersoftware.in>
2018-02-07 20:41:47 -0800

    Added custom functionality for showing increments & labels, snap the dial and resize dial text

--- src/js/dial.js
+++ src/js/dial.js
@@ -14,6 +14,7 @@
    * @param {Number} outerRadius
    * @param {Number} startAngle
    * @param {Number} endAngle
+   * @param {Boolean} clickable
    */
   var Knob = function(element, initialValue, innerRadius, outerRadius, startAngle, endAngle, clickable) {
     this.element = element;
@@ -21,11 +22,18 @@
     this.radians = this.convertToRadians(initialValue);
     this.innerRadius = innerRadius;
     this.outerRadius = outerRadius;
-    this.startAngle = startAngle,
-    this.endAngle = endAngle,
-    this.offset = this.outerRadius + 20;
+    this.startAngle = startAngle;
+    this.endAngle = endAngle;
+    this.offset = this.outerRadius + 40;
     this.inDrag = false;
     this.clickable = clickable;
+    this.minValue = 0;
+    this.maxValue = 100;
+    this.prefix = null;
+    this.suffix = null;
+    this.noOfSegments = 0;
+    this.noOfDecimals = 0;
+    this.allowTosnap = false;
   };
 
   /**
@@ -111,6 +119,8 @@
     var svg = d3.select(that.element)
       .append('svg');
 
+    drawArc(that.interactArc, 'baseArc');
+
     var changeElem = drawArc(that.changeArc, 'changeArc');
     var valueElem = drawArc(that.valueArc, 'valueArc');
 
@@ -121,7 +131,7 @@
     drawArc(that.interactArc, 'interactArc', clickInteraction, dragBehavior);
 
     if (isAnimated) {
-      animate(that.convertToRadians(that.startAngle, 360), that.convertToRadians(that.value, 100, that.endAngle, that.startAngle));
+      that.animate(that.convertToRadians(that.startAngle, 360), that.convertToRadians(that.value, 100, that.endAngle, that.startAngle));
     } else {
       that.changeArc.endAngle(this.convertToRadians(this.value, 100, this.endAngle, this.startAngle));
       changeElem.attr('d', that.changeArc);
@@ -153,21 +163,6 @@
       return elem;
     }
 
-    function animate(start, end) {
-      valueElem
-        .transition()
-        .ease('bounce')
-        .duration(1000)
-        .tween('', function() {
-          var i = d3.interpolate(start, end);
-          return function(t) {
-            var val = i(t);
-            valueElem.attr('d', that.valueArc.endAngle(val));
-            changeElem.attr('d', that.changeArc.endAngle(val));
-          };
-        });
-    }
-
     function dragInteraction() {
       if(that.clickable){
         that.inDrag = true;
@@ -195,6 +190,11 @@
         delta = 270;
       }
       radians = ((delta-that.startAngle) + arc) * (Math.PI/180);
+      if(that.allowToSnap && that.noOfSegments > 0) {
+        var angle = radians * (180 / Math.PI);
+        angle = that.getClosestAngle(angle);
+        radians = angle * (Math.PI / 180);
+      }
       that.value = that.convertFromRadians(radians, 100, that.endAngle, that.startAngle);
       if(that.value >= 0 && that.value <= 100) {
         updateFn(that.value);
@@ -204,26 +204,348 @@
           that.changeArc.endAngle(that.convertToRadians(that.value, 100, that.endAngle, that.startAngle));
           d3.select(that.element).select('#changeArc').attr('d', that.changeArc);
         }
-        d3.select(that.element).select('#text').text(that.value);
+        d3.select(that.element).select('#text').text(that.getDisplayValue(that.convertToMinMaxValue(that.value)));
+        that.adjustCenterText();
       }
     }
+  };
+
+  /**
+   * Redraw the dial
+   *
+   * @param {Boolean} animate
+   *
+   * @return {void}
+   */
+  Knob.prototype.reDraw = function (animate) {
+    d3.select(this.element).select('svg').remove();
+    this.draw(function(){}, animate);
+  };
+
+  /**
+   * Animate the dial
+   *
+   * @param {Number} start
+   * @param {Number} end
+   *
+   * @return {void}
+   */
+  Knob.prototype.animate = function(start, end) {
+    var that = this;
+    var valueElem = d3.select(that.element).select('#valueArc');
+    var changeElem = d3.select(that.element).select('#changeArc');
+    valueElem
+      .transition()
+      .ease('bounce')
+      .duration(1000)
+      .tween('', function() {
+        var i = d3.interpolate(start, end);
+        return function(t) {
+          var val = i(t);
+          valueElem.attr('d', that.valueArc.endAngle(val));
+          changeElem.attr('d', that.changeArc.endAngle(val));
+        };
+      });
   };
 
   /**
    * Set the value of the gauge to something new.
    *
    * @param {Number} newValue
+   *
+   * @return {void}
    */
   Knob.prototype.setValue = function(newValue) {
+    newValue = this.convertFromMinMaxValue(newValue);
     if ((!this.inDrag) && this.value >= 0 && this.value <= 100) {
       var radians = this.convertToRadians(newValue, 100, this.endAngle, this.startAngle);
       this.value = newValue;
       this.changeArc.endAngle(radians);
-      d3.select(this.element).select('#changeArc').attr('d', this.changeArc);
       this.valueArc.endAngle(radians);
-      d3.select(this.element).select('#valueArc').attr('d', this.valueArc);
-      d3.select(this.element).select('#text').text(newValue);
+      this.animate(this.convertToRadians(this.startAngle, 360), radians);
+      newValue = this.convertToMinMaxValue(newValue);
+      d3.select(this.element).select('#text').text(this.getDisplayValue(newValue));
+      this.adjustCenterText();
     }
+  };
+
+  /**
+   * Set the minimum value
+   *
+   * @param {Number} value
+   *
+   * @return {void}
+   */
+  Knob.prototype.setMinValue = function(value) {
+    this.minValue = value;
+  };
+
+  /**
+   * Set the maximum value
+   *
+   * @param {Number} value
+   *
+   * @return {void}
+   */
+  Knob.prototype.setMaxValue = function(value) {
+    this.maxValue = value;
+  };
+
+  /**
+   * Set the prefix
+   *
+   * @param {String} prefix
+   *
+   * @return {void}
+   */
+  Knob.prototype.setPrefix = function(prefix) {
+    this.prefix = prefix;
+  };
+
+  /**
+   * Set the suffix
+   *
+   * @param {String} suffix
+   *
+   * @return {void}
+   */
+  Knob.prototype.setSuffix = function(suffix) {
+    this.suffix = suffix;
+  };
+
+  /**
+   * Allow to snap
+   *
+   * @param {Boolean} snap
+   *
+   * @return {void}
+   */
+  Knob.prototype.isAllowToSnap = function(snap) {
+    this.allowToSnap = snap;
+  };
+
+  /**
+   * Allow to show the dial center text
+   *
+   * @param {Boolean} flag
+   *
+   * @return {void}
+   */
+  Knob.prototype.isShowCenterText = function(flag) {
+    if(flag === false) {
+      d3.select(this.element).select('#text').style('display', 'none');
+    }
+  };
+
+  /**
+   * Set number of segments
+   *
+   * @param {Number} noOfSegments
+   *
+   * @return {void}
+   */
+  Knob.prototype.setNoOfSegments = function(noOfSegments) {
+    this.noOfSegments = noOfSegments;
+  };
+
+  /**
+   * Format the display value with prefix and suffix
+   *
+   * @param {String} value
+   *
+   * @return {String}
+   */
+  Knob.prototype.getDisplayValue = function(value) {
+    if(this.prefix) {
+      value = this.prefix + value;
+    }
+    if(this.suffix) {
+      value += this.suffix;
+    }
+    return value;
+  };
+
+  /**
+   * Convert [0-100] value to [Min-Max] value
+   *
+   * @param {Number} value
+   *
+   * @return {Number}
+   */
+  Knob.prototype.convertToMinMaxValue = function(value) {
+    return parseFloat(value * ((this.maxValue - this.minValue) / 100) + parseFloat(this.minValue)).toFixed(this.noOfDecimals);
+  };
+
+  /**
+   * Convert [Min-Max] value to [0-100] value
+   *
+   * @param {Number} value
+   *
+   * @return {Number}
+   */
+  Knob.prototype.convertFromMinMaxValue = function (value) {
+    return parseFloat((value - this.minValue) / (this.maxValue - this.minValue) * 100).toFixed(this.noOfDecimals);
+  };
+
+  /**
+   * Get closest angle for given angle
+   *
+   * @param {Number} angle
+   *
+   * @return {Number}
+   */
+  Knob.prototype.getClosestAngle = function(angle) {
+    var closest, smallestDiff, currentDiff;
+    var segments = d3.range(0, 360, 360 / this.noOfSegments);
+    if(segments.indexOf(angle) > -1) {
+      return angle;
+    }
+
+    // If its last segment
+    if(segments.slice(-1)[0] < angle) {
+      return 360;
+    }
+
+    smallestDiff = Math.abs(angle - segments[0]);
+    closest = 0;
+
+    for (var i = 1; i < segments.length; i++) {
+      currentDiff = Math.abs(angle - segments[i]);
+      if (currentDiff < smallestDiff) {
+        smallestDiff = currentDiff;
+        closest = i;
+      }
+    }
+    return segments[closest];
+  };
+
+  /**
+   * Find and Set decimal point from Min Value, Max Value and Correct value
+   *
+   * @param {Number} minValue
+   * @param {Number} maxValue
+   * @param {Number} correctValue
+   *
+   * @return {void}
+   */
+  Knob.prototype.setNoOfDecimals = function (minValue, maxValue, correctValue) {
+    this.noOfDecimals = Math.max(checkForDecimal(minValue), checkForDecimal(maxValue), checkForDecimal(correctValue));
+
+    function checkForDecimal(value) {
+      if (value - Math.floor(value) !== 0) {
+        var str = value.toString();
+        return str.length - 1 - str.indexOf('.');
+      }
+      return 0;
+    }
+  };
+
+  /**
+   * Set the segment lines and its labels.
+   *
+   * @param {Number} noOflabels
+   */
+  Knob.prototype.setSegments = function (noOflabels) {
+    var that = this;
+    var magicValue = 5;
+    var numberOfSegments = this.noOfSegments;
+    var outerRadius = that.outerRadius;
+    var radians = (Math.PI * 2) / numberOfSegments;
+    var degrees = 360 / numberOfSegments;
+    var segments = d3.range(this.minValue, this.maxValue, (this.maxValue - this.minValue) / numberOfSegments);
+    var labelItems = [];
+
+    var svg = d3.select(this.element).select('svg');
+    var arc = d3.svg.arc()
+      .innerRadius(outerRadius + (2 * magicValue))
+      .outerRadius(outerRadius + (2 * magicValue))
+      .startAngle(function (d) {
+        return radians * d;
+      }).endAngle(function (d) {
+        return radians * d;
+      });
+
+    var ga = svg.append('g')
+      .attr('class', 'a axis')
+      .attr('transform', 'translate(30,' + that.offset + ')')
+      .selectAll('g')
+      .data(d3.range(numberOfSegments))
+      .enter()
+      .append('g')
+      .attr('transform', function (d) {
+        return 'translate(' + arc.centroid(d) + ')';
+      });
+
+    ga.append('line')
+      .attr('x2', outerRadius)
+      .attr('x1', outerRadius + (2 * magicValue))
+      .attr('transform', function (d) {
+        return 'rotate(' + (d * degrees - 90) + ' ' + (outerRadius + (2 * magicValue)) + ' 0)';
+      });
+
+    if(parseInt(noOflabels) > 0) {
+      // Collecting increments label
+      var mod = Math.floor(numberOfSegments / noOflabels);
+      for (var y=0; y <= numberOfSegments; y += mod) {
+        labelItems.push(y);
+        if(labelItems.length >= noOflabels) {
+          break;
+        }
+      }
+
+      // Set increments label text
+      ga.append('text')
+        .attr('x', outerRadius + (3 * magicValue))
+        .attr('dy', '.35em').attr('transform', function(d) {
+          var angle, tx, ty;
+          angle = d * degrees;
+          if (angle === 0) {
+            tx = ty = -(2 * magicValue);
+          } else if (angle === 90) {
+            tx = ty = 0;
+          } else if (angle === 180) {
+            tx = -(3 * magicValue);
+            ty = 2 * magicValue;
+          } else if (angle === 270) {
+            tx = -(6 * magicValue);
+            ty = 0;
+          } else if ((angle < 90 && angle > 0)) {
+            tx = ty = -1 * magicValue;
+          } else if ((angle < 180 && angle > 90)) {
+            tx = -1 * magicValue;
+            ty = magicValue;
+          } else if ((angle < 270 && angle > 180)) {
+            tx = -(5 * magicValue);
+            ty = magicValue;
+          } else if ((angle < 360 && angle > 270)) {
+            tx = -(5 * magicValue);
+            ty = -1 * magicValue;
+          }
+          return 'translate(' + tx + ',' + ty + ')';
+        }).style('text-anchor', function(d) {
+          return d < 360 && d > 180 ? 'end' : null;
+        }).text(function(d) {
+          // Don't set increments label for some increment
+          if (labelItems.indexOf(d) === -1) {
+            return '';
+          }
+          return that.getDisplayValue(segments[d].toFixed(that.noOfDecimals));
+        });
+    }
+  };
+
+  /**
+   * Align center text as per font size
+   *
+   * @return {void}
+   */
+  Knob.prototype.adjustCenterText = function () {
+    var textElement = d3.select(this.element).select('#text').node();
+    var textBox = textElement.getBBox();
+    var fontSize = parseFloat(window.getComputedStyle(textElement).fontSize);
+    var tx = this.offset - (textBox.width / 2);
+    var ty = this.offset - (textBox.height / 2) + fontSize;
+    d3.select(this.element).select('#text').attr('transform', 'translate(' + tx + ', ' + ty + ')');
   };
 
   gmd.Knob = Knob;
@@ -235,16 +557,20 @@
         value: '='
       },
       link: function (scope, element, attrs) {
-        var innerRadius = parseInt(attrs.innerRadius, 10) || 60,
-          outerRadius = parseInt(attrs.outerRadius, 10) || 100,
+        var clientWidth = document.documentElement.clientWidth,
+          innerRadius = clientWidth < 400 ? 50 : 100,
+          outerRadius = clientWidth < 400 ? 100 : 150,
           startAngle = parseInt(attrs.startAngle, 10) || 0,
           endAngle = parseInt(attrs.endAngle, 10) || 360,
           clickable = (attrs.clickable === 'false') ? false : true,
           knob = new gmd.Knob(element[0], scope.value, innerRadius, outerRadius, startAngle, endAngle, clickable);
 
+        scope.$parent.knob = knob;
+
         function update(value) {
           scope.$apply(function() {
             scope.value = value;
+            scope.$parent.value = value;
           });
         }
 
